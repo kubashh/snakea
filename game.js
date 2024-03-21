3d60406c033283c79ef0b8bb1154f3f37ab7e16e
kubashh <146523311+kubashh@users.noreply.github.com>
2024-03-21 10:39:13 +0000

    1.2f

--- game.js
+++ game.js
@@ -159,7 +159,7 @@ async function draw() {
 function render() {
   drawBox(0, 0, window.innerWidth, window.innerHeight, "black");
 
-  //draw();
+  draw();
 }
 
 function drawBox(x, y, w, h, color = "white")  {
@@ -222,7 +222,6 @@ function startNewGame(nick, color) {
   newSnake(nick, color);
   let maxfps = 5;
   renderLoop = setInterval(render, 1000 / maxfps);
-  draw();
 
   document.addEventListener('keydown', (event) => {
     if(inGame) {
