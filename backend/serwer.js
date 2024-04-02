a34e05cb8576075ed33f3c98d013152e42725f94
kubashh <146523311+kubashh@users.noreply.github.com>
2024-04-02 10:05:42 +0000

    2.5p delete

--- backend/serwer.js
+++ backend/serwer.js
@@ -1,25 +1,25 @@
-const WebSocket = require('ws');
+const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({ port: 8888 });
+const wss = new WebSocket.Server({ port: 8888 })
 
 wss.on("connection", function connection(ws) {
   ws.on("message", function incoming(message) {
-    let data = JSON.parse(message);
+    let data = JSON.parse(message)
     switch(data.type) {
       case "newSnake":
         if(isGoodNewSnakeData(data.nick, data.color)) {
-          new Snake(data.nick, data.color);
+          new Snake(data.nick, data.color)
           ws.send(JSON.stringify({
             type: "snakeSpowned"
-          }));
+          }))
         }
-        break;
+        break
       
       case "board":
-        let snake = getSnakeByNick(data.nick);
-        let head = null;
+        let snake = getSnakeByNick(data.nick)
+        let head = null
         if(snake) {
-          head = snake.head();
+          head = snake.head()
         }
         ws.send(JSON.stringify({
           type: "board",
@@ -29,105 +29,105 @@ wss.on("connection", function connection(ws) {
           snakes: snakes,
           snakesCount: snakes.length,
           topTen: topTen
-        }));
-        break;
+        }))
+        break
 
       case "direction":
         if(data.nick && data.direction !== undefined) {
-          changeDirection(data.nick, data.direction);
+          changeDirection(data.nick, data.direction)
         }
-        break;
+        break
     }
-  });
-});
+  })
+})
 
 
 
 
-const mapSize = 80;
-let board = [ [ "black" ] ];
-let snakes = [];
-let apples = [];
-let topTen = [ "", "", "", "", "", "", "", "", "", "" ];
+const mapSize = 80
+let board = [ [ "black" ] ]
+let snakes = []
+let apples = []
+let topTen = [ "", "", "", "", "", "", "", "", "", "" ]
 
 
 
 class Snake {
   constructor(nick = "empty", color = "black") {
-    this.direction = Math.floor(Math.random() *  4);
-    this.nick = nick;
-    this.color = color;
-    let a = freePos(6);
-    this.body = [a, a, a];
-    snakes.push(this);
+    this.direction = Math.floor(Math.random() *  4)
+    this.nick = nick
+    this.color = color
+    let a = freePos(6)
+    this.body = [a, a, a]
+    snakes.push(this)
   }
 
   move = () => {
-    let next = { x: this.body[this.body.length - 1].x, y: this.body[this.body.length - 1].y };
+    let next = { x: this.body[this.body.length - 1].x, y: this.body[this.body.length - 1].y }
     switch(this.direction) {
       case 0:
-        next.y -= 1;
-        break;
+        next.y -= 1
+        break
       case 1:
-        next.x -= 1;
-        break;
+        next.x -= 1
+        break
       case 2:
-        next.y += 1;
-        break;  
+        next.y += 1
+        break
       case 3:
-        next.x += 1;
-        break;
+        next.x += 1
+        break
     }
     if(next.x < 0 || next.y < 0 || next.x >= mapSize || next.y >= mapSize) {
-      deleteSnake(this);
-      return;
+      deleteSnake(this)
+      return
     }
     for(let snake of snakes) {
       if(snake.nick != this.nick) {
         for(let bodyElement of snake.body) {
           if(bodyElement.x == next.x && bodyElement.y == next.y) {
-            deleteSnake(this);
-            return;
+            deleteSnake(this)
+            return
           }
         }
       }
     }
-    this.body.push(next);
+    this.body.push(next)
     for(let apple of apples) {
       if(next.x == apple.x && next.y == apple.y) {
-        apples.splice(apples.indexOf(apple), 1);
-        return;
+        apples.splice(apples.indexOf(apple), 1)
+        return
       }
     }
-    this.body.shift();
+    this.body.shift()
   }
 
   head = () => {
-    return this.body[this.body.length - 1];
+    return this.body[this.body.length - 1]
   }
 
   changeDirection = (a) => {
     switch(a) {
       case 0:
         if(this.body[this.body.length - 2].y != this.head().y - 1) {
-          this.direction = a;
+          this.direction = a
         }
-        break;
+        break
       case 1:
         if(this.body[this.body.length - 2].x != this.head().x - 1) {
-          this.direction = a;
+          this.direction = a
         }
-        break;
+        break
       case 2:
         if(this.body[this.body.length - 2].y != this.head().y + 1) {
-          this.direction = a;
+          this.direction = a
         }
-        break;
+        break
       case 3:
         if(this.body[this.body.length - 2].x != this.head().x + 1) {
-          this.direction = a;
+          this.direction = a
         }
-        break;
+        break
     }
   }
 }
@@ -137,163 +137,155 @@ class Snake {
 function deleteSnake(snake) {
   for(let bodyElement of snake.body) {
     if(bodyElement != snake.head() && chance(0.4)) {
-      apples.push(bodyElement);
+      apples.push(bodyElement)
     }
   }
-  snakes.splice(snakes.indexOf(snake), 1);
+  snakes.splice(snakes.indexOf(snake), 1)
 }
 
 function generateApple() {
   if(apples.length < mapSize / 6) {
-    apples.push(freePos(3));
+    apples.push(freePos(3))
   }
 }
 
 function chance(a) {
   if(Math.random() < a) {
-    return true;
+    return true
   }
-  return false;
+  return false
 }
 
 function getSnakeByNick(nick) {
   for(let snake of snakes) {
     if(snake.nick === nick) {
-      return snake;
+      return snake
     }
   }
-  return null;
+  return null
 }
 
 function changeDirection(nick, direction) {
-  let snake = getSnakeByNick(nick);
+  let snake = getSnakeByNick(nick)
   if(snake) {
-    snake.changeDirection(direction);
+    snake.changeDirection(direction)
   }
 }
 
 function randInt(a, b) {
-  return Math.floor(a + Math.random() * Math.floor(b - a));
+  return Math.floor(a + Math.random() * Math.floor(b - a))
 }
 
 function freePos(a = 0) {
-  let pos = { x: randInt(a, mapSize - a), y: randInt(a, mapSize - a) };
+  let pos = { x: randInt(a, mapSize - a), y: randInt(a, mapSize - a) }
   
-  let b = true;
+  let b = true
   while(b) {
-    b = false;
+    b = false
     for(let snake of snakes) {
       for(let snakebody of snake.body) {
         if(pos.x == snakebody.x && pos.y == snakebody.y) {
-          pos = { x: randInt(a, mapSize - a), y: randInt(a, mapSize - a) };
-          b = true;
+          pos = { x: randInt(a, mapSize - a), y: randInt(a, mapSize - a) }
+          b = true
         }
       }
     }
     for(let apple of apples) {
       if(pos.x == apple.x && pos.y == apple.y) {
-        pos = { x: randInt(a, mapSize - a), y: randInt(a, mapSize - a) };
-        b = true;
+        pos = { x: randInt(a, mapSize - a), y: randInt(a, mapSize - a) }
+        b = true
       }
     }
   }
 
-  return pos;
+  return pos
 }
 
 
 
 function isGoodNewSnakeData(nick, color) {
   if(!isGoodNick(nick) || !isGoodColor(color)) {
-    return false;
+    return false
   }
 
-  return true;
+  return true
 }
 
 function isGoodNick(nick) {
   for(let snake of snakes) {
     if(snake.nick == nick) {
-      return false;
+      return false
     }
   }
 
-  return true;
+  return true
 }
 
 function isGoodColor(color) {
   for(let snake of snakes) {
     if(snake.color == color) {
-      return false;
+      return false
     }
   }
 
-  return true;
+  return true
 }
 
 
 function updateTopTen() {
   for(let i = 0; i < 10; i++) {
     if(snakes[i]) {
-      let snake = snakes[i];
+      let snake = snakes[i]
       topTen[i] = {
         nick: "#" + (i + 1) + "   " + snake.nick,
         score: snake.body.length,
         color: snake.color
-      };
+      }
     } else {
       topTen[i] = {
         nick: "",
         score: "",
         color: ""
-      };
+      }
     }
   }
 }
 
 
-//let lastTime = new Date();
-
 function update() {
   for(let snake of snakes) {
-    snake.move();
+    snake.move()
   }
 
   if(chance(0.06)) {
-    generateApple();
+    generateApple()
   }
 
   // odswierzanie planszy
-  board = Array.from({ length: mapSize }, () => Array(mapSize).fill("black"));
+  board = Array.from({ length: mapSize }, () => Array(mapSize).fill("black"))
 
   for(let snake of snakes) {
     for(let bodyElement of snake.body) {
-      board[bodyElement.x][bodyElement.y] = snake.color;
+      board[bodyElement.x][bodyElement.y] = snake.color
     }
   }
   
   for(let apple of apples) {
-    board[apple.x][apple.y] = "yellow";
+    board[apple.x][apple.y] = "yellow"
   }
 
   //sort
   for(let i = 0; i < snakes.length - 1; i++) {
     for(let j = i + 1; j < snakes.length; j++) {
       if(snakes[j].body.length > snakes[i].body.length) {
-        let temp = snakes[i];
-        snakes[i] = snakes[j];
-        snakes[j] = temp;
+        let temp = snakes[i]
+        snakes[i] = snakes[j]
+        snakes[j] = temp
       }
     }
   }
   
-  updateTopTen();
-
-  /*let now = new Date();
-  let timer = now - lastTime;
-  lastTime = now;
-
-  console.log(timer);*/
+  updateTopTen()
 }
 
-setInterval(update, 100);
+setInterval(update, 100)
